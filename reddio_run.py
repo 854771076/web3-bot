b7b5448e104f2339ec03ca169d1f9b3d204414d1
fuyang <fuyang854771076@gmail.com>
2025-02-20 13:39:45 +0800

    1

--- reddio_run.py
+++ reddio_run.py
@@ -1,5 +1,6 @@
 from core import TakerBotManager,ReddioBotManager,BlockingScheduler
 if __name__ == '__main__':
+    #邀请码任务平台网站 soneiumevent.unemeta.com/?invitationCode=true&codes=39YH8
     config_path='./config/config_reddio.json'
     manager=ReddioBotManager(config_path)
     manager.run()
