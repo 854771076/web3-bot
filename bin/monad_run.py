49df50e0b9da3f354b9f07152d43a347eb074356
fuyang <fuyang854771076@gmail.com>
2025-03-05 16:03:37 +0800

    README_MONAD

--- bin/monad_run.py
+++ bin/monad_run.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     config_path='./config/config_monad.json'
     manager=MonadBotManager(config_path)
     manager.run()
+    # manager.run_single(manager.accounts[2])
 
     # 执行后每过24小时执行一次
     # scheduler = BlockingScheduler()
